f2760afc2a042b91ed2eff7f00f56396668a35d1
Revan Midha <revi.midha@gmail.com>
2026-03-26 00:51:43 +0530

    improvements

--- search 2d sorted matrix.py
+++ search 2d sorted matrix.py
@@ -1,24 +1,24 @@
 def search(matrix, target):
     
-    n1 = len(matrix)
-    n2 = len(matrix[0])
+    rows = len(matrix)
+    cols = len(matrix[0])
 
     i = 0
-    j = n2 - 1
+    j = cols - 1
 
-    while 0 <= i < n1 and 0 <= j < n2:
-        helper = matrix[i][j]
+    while i < rows and j >= 0:
+        cur_val = matrix[i][j]
 
-        if helper == target:
+        if cur_val == target:
             return True
-        elif helper > target:
+        elif cur_val > target:
             j -= 1
         else:
             i += 1
 
     return False
 
-mat = [[1,3,5,7],[10,11,16,20],[23,30,34,60]]
+mat = [[1, 3, 5, 7],[10, 11, 16, 20],[23, 30, 34, 60]]
 target = 13
 
 print(search(mat, target))
